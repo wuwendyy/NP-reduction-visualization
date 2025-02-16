8d8e47a4a9a0fddfe51b7ae0f4d01f3f280b89e7
Alice <aliceyzh@usc.edu>
2025-02-15 20:06:08 -0800

    basic Formula parsing done

--- Elements/element.py
+++ Elements/element.py
@@ -4,6 +4,7 @@ import numpy as np
 import math
 import pygame
 
+
 class Element:
     # display to pygame
     @abstractmethod
@@ -25,7 +26,9 @@ class Graph(Element):
     bounding box: 2*2 matrix of [x_min, y_min],[x_max, y_max]
     node radius: radius of (most) nodes 
     '''
-    def __init__(self, nodes = set(), edges = set(), groups = [], bounding_box = np.array([[50,50],[550,550]]), node_radius = 20):
+
+    def __init__(self, nodes=set(), edges=set(), groups=[], bounding_box=np.array([[50, 50], [550, 550]]),
+                 node_radius=20):
         self.nodes = nodes
         self.edges = edges
         self.groups = groups
@@ -57,15 +60,17 @@ class Graph(Element):
     '''
     Helper function in determine the node positions 
     '''
+
     def determine_node_positions(self):
         if len(self.groups) != 0:
             # has group assignment 
             self.determine_node_positions_by_groups()
-        return 
+        return
 
     '''
     Helper function for 2d vector rotations  
     '''
+
     def rotate_vector_np(self, vector, angle_degrees):
         angle_radians = np.radians(angle_degrees)
         rotation_matrix = np.array([
@@ -77,45 +82,48 @@ class Graph(Element):
     '''
     Helper function to get node by id 
     '''
+
     def get_node_by_id(self, id):
         return self.node_dict.get(id)
-    
+
     '''
     Create dictionary for node loop up
     '''
+
     def create_node_dictionary(self):
         self.node_dict = {node.node_id: node for node in self.nodes}
 
-    def determine_node_positions_by_groups(self, group_size = 200, gap = 40):
+    def determine_node_positions_by_groups(self, group_size=200, gap=40):
         # assign group areas
         # TODO: count in gaps in distributing the bounded space 
         group_num = len(self.groups)
-        groups_per_row = max(math.ceil(math.sqrt(group_num)),4)
+        groups_per_row = max(math.ceil(math.sqrt(group_num)), 4)
         groups_per_col = math.ceil(group_num / groups_per_row)
 
-        bounding_box_dim = self.bounding_box[1,:] - self.bounding_box[0,:]
-        width_per_groups = min((bounding_box_dim[0]/groups_per_row), (bounding_box_dim[1]/groups_per_col))
+        bounding_box_dim = self.bounding_box[1, :] - self.bounding_box[0, :]
+        width_per_groups = min((bounding_box_dim[0] / groups_per_row), (bounding_box_dim[1] / groups_per_col))
         width_per_groups = min(width_per_groups, group_size)
         radius = width_per_groups / 2.
         # loop through each groups 
         r = 0
         c = 0
-        start_vector = np.array([0,-1.*radius])
+        start_vector = np.array([0, -1. * radius])
         for group in self.groups:
-            center = np.array([gap * c, gap * r]) + self.bounding_box[0,:] + np.array([(0.5+c)*width_per_groups, (0.5+r)*width_per_groups])
+            center = np.array([gap * c, gap * r]) + self.bounding_box[0, :] + np.array(
+                [(0.5 + c) * width_per_groups, (0.5 + r) * width_per_groups])
             angle = 360. / len(group)
             for i in range(len(group)):
                 displacement = self.rotate_vector_np(start_vector, i * angle)
                 node_new_pos = center + displacement
                 group[i].location = node_new_pos
-            
+
             c += 1
             if c == groups_per_row:
                 r += 1
                 c = 0
 
     def display(self, screen):
-         # Draw edges
+        # Draw edges
         for edge in self.edges:
             start_pos = edge.node1.location
             end_pos = edge.node2.location
@@ -127,18 +135,59 @@ class Graph(Element):
             # Optionally draw node name in the center
             font = pygame.font.SysFont(None, 24)
             text_surface = font.render(node.name, True, (255, 255, 255))
-            text_rect = text_surface.get_rect(center = (int(node.location[0]), int(node.location[1])))
+            text_rect = text_surface.get_rect(center=(int(node.location[0]), int(node.location[1])))
             screen.blit(text_surface, text_rect)
 
 
+# Example Formula
+"""
+formula.clauses = [ #(var, is_negated, clause_idx)
+    [(1, False, 1), (2, True, 1), (3, False, 1)],
+    [(2, False, 2), (3, True, 2), (4, False, 2)],
+    [(1, False, 3), (2, True, 3), (4, False, 3)]
+]
+"""
+
 
 class Formula(Element):
-    def __init__(self):
-        self.clauses = set()
-        self.variables = set()
+    clauses = []
+
+    def parse(self, filename):
+        counter = 0
+        with open(filename, "r") as f:
+            x = f.read().split("OR")
+            for c in x:
+                counter += 1
+                c = c.strip()[1:-1]  # remove the parentheses
+                clause = Clause(counter)
+                v = c.split()
+                i = 0
+                while i < len(v):
+                    negate = False
+                    if v[i] == "NOT":
+                        negate = True
+                        i += 1
+                    var = Variable(v[i], negate)
+                    clause.add_variable(var)
+                    i += 2  # skip the AND part
+                self.clauses.append(clause)
+        f.close()
+        print(self.clauses)
+
+# return as a list of list of tuples
+    def get_as_list(self):
+        outer_list = []
+        for clause in self.clauses:
+            inner_list = []
+            for var in clause.variables:
+                tup = (var.var_id, var.negate, clause.clause_id)
+                inner_list.append(tup)
+            outer_list.append(inner_list)
+        return outer_list
+
+    def evaluate(self):
+        for c in self.clauses:
+            pass
 
     def display(self):
-        pass
-
-    def parse(self):
         pass
--- Elements/helpers.py
+++ Elements/helpers.py
@@ -1,6 +1,8 @@
 import numpy as np
+
+
 class Node:
-    def __init__(self, node_id, name, selected="false", color=(0, 0, 255), location=np.array([0,0])):
+    def __init__(self, node_id, name, selected="false", color=(0, 0, 255), location=np.array([0, 0])):
         self.node_id = node_id
         self.name = name
         self.selected = selected
@@ -17,3 +19,39 @@ class Edge:
         self.node2 = node2
         self.selected = selected
         self.color = color
+
+
+class Variable:
+    def __init__(self, var_id, negate):
+        self.var_id = var_id
+        self.negate = negate
+
+
+class Clause:
+    variables = []
+
+    def __init__(self, clause_id: int):
+        self.variables = []
+        self.clause_id = clause_id
+
+    def add_variable(self, variable: Variable):
+        self.variables.append(variable)
+
+    def test_print(self):
+        print(f"Clause: {self.clause_id}")
+        for v in self.variables:
+            print(f"ID: {v.var_id}, {v.negate}")
+
+    # given a solution, evaluate if the clause is true
+    def evaluate(self, solution):
+        result = True
+        # for i in range(len(self.variables)):
+        #     v = self.variables[i]
+        #     b = solution[i]
+        #     if v.negate:
+        #         result = result or b
+        #     else:
+        #         result = result or not b
+
+        return result
+
